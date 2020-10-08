e23f65e541f4de75128ba9bac45258ff52b534e3
Jonathan Keefe <jonathan.keefe@daviestech.org>
2020-10-08 19:55:49 +0000

    Question 5

--- Question5.java
+++ Question5.java
@@ -6,6 +6,30 @@ class Question5 {
         /* Write your code here */
       Scanner scan = new Scanner(System.in);
 
+      
+      double user1;
+      int ones;
+      int tenths;
+      int num;
+      int num2;
+    
+
+      System.out.println("Please Input a decimal number");
+      user1 = scan.nextDouble();
+
+      double a = user1 * 10;
+      double b = user1 * 100;
+
+      ones = (int)a;
+      tenths = (int)b;
+
+      num = ones % 10;
+      num2 =tenths % 10;
+      
+      
+      
+
+      System.out.println("Answer: " + num+ " "+ num2);
 
 
 
--- run_button.sh
+++ run_button.sh
@@ -1,2 +1,2 @@
-javac Question4.java
-java Question4
+javac Question5.java
+java Question5
